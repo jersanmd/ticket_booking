c2d78f9c878f9e5a43d4d25ed17266eca4024309
Jersan Balago <1stjersan@gmail.com>
2022-09-06 17:47:07 +0800

    DONE: Upcoming Flights

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_booking/screens/bottom_bar.dart';
+import 'package:ticket_booking/utils/app_styles.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Ticket Booking App',
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primaryColor: primary
       ),
       home: const BottomBar()
     );
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,101 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ticket_booking/screens/ticket_view.dart';
+import 'package:ticket_booking/utils/app_layout.dart';
+import 'package:ticket_booking/utils/app_styles.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children:[
+                const Gap(40),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Good Morning', style: Styles.headlineStyle3),
+                          const Gap(5),
+                          Text('Book Tickets', style: Styles.headlineStyle1),
+                        ],
+                      ), 
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Container(
+                          width: 34,
+                          height: 34,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/logo.png'),
+                              fit: BoxFit.fill,
+                            )
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const Gap(25),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                    child: Row(children: [
+                      const Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205)),
+                      Text('Search', style: Styles.headlineStyle4)
+                    ],),
+                  ),
+                  const Gap(40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Upcoming Flights', style: Styles.headlineStyle2),
+                      InkWell(
+                        onTap: () {},
+                        child: Text('View all', style: Styles.textStyle.copyWith(color: Styles.primaryColor)))
+                    ],
+                  )
+                  
+              ],
+            ),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children: const [
+                TicketView(),
+                TicketView(),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
